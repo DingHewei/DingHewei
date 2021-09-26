03f374c966dd1f55ee343a9fa297aeed53698fc7
DingHewei <89379407+DingHewei@users.noreply.github.com>
2021-09-26 15:42:51 +0800

    Create haiguihuatu.py
    一个基本画图

---
+++ haiguihuatu.py
@@ -0,0 +1,17 @@
+import turtle          #导入turtle模块
+turtle.write('wc')     #输入字符
+turtle.showturtle()    #显示箭头
+turtle.forward(50)     #向前走50
+turtle.color('red')    #改变箭头颜色
+turtle.left(90)       #箭头左转90度
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.goto(0,100)   #直接到点（0,50）
+turtle.penup()     #抬笔
+turtle.goto(50,100)#抬笔到点（50,100）
+turtle.pendown()
+turtle.pencolor('blue')
+turtle.forward(20)
+turtle.circle(100)
+turtle.done()   #界面保持显示
